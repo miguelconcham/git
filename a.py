3cc7d5ac6f028fa9c25ca439c148c54f847209df
Miguel Concha <miguelconcham@gmail.com>
2018-03-07 16:32:47 +0100

    final version

--- a.py
+++ a.py
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-<<<<<< HEAD
-sdasdasdadsjdljddla
-=======
-sjdlakjdlaskjdlakjdsflakjdfhlakjsdaf porquéeeeeee
->>>>>>> worldrev
-=======
-sjdlakjdlaskjdlakjdsflakjdfhlakjsdaf
-<<<<<<< HEAD
-nuevos cambios de worldrev
->>>>>>> worldre
+
 MASTER QUIERE CAMBIAR TAMBIEN
-=======
-nuevos cambios de worldre
-nuevos cambios: rasterplot
->>>>>>> worldrev
+
